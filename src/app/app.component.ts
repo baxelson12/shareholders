88443f7c3e74c95d05c1217d30756155e9bad8c8
Brad Axe <bradaxelson@gmail.com>
2021-06-25 00:33:25 -0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { EMPTY, from, Observable } from 'rxjs';
+import {
+  catchError,
+  map,
+  share,
+  startWith,
+  switchMap,
+  switchMapTo,
+  take,
+  tap,
+} from 'rxjs/operators';
+import abi from '../assets/abi.json';
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  status: string = '';
+  web3 = new Web3((window as any).web3.currentProvider);
+  contract = new this.web3.eth.Contract(
+    abi as AbiItem[],
+    '0xbCFD134f83431260eE210042d40cFDC94a90857D'
+  );
+  eth = (window as any).ethereum;
+  account$: Observable<string> = from<string[]>(
+    this.eth.request({ method: 'eth_requestAccounts' })
+  ).pipe(
+    map(([account]) => account),
+    share(),
+    catchError((e) => {
+      console.warn('Wallet lookup fail', e);
+      return EMPTY;
+    })
+  );
+
+  release(): void {
+    this.status = '';
+    this.account$
+      .pipe(
+        tap(() => (this.status = 'Wait')),
+        switchMap((addr) =>
+          from(this.contract.methods.release(addr).send({ from: addr }))
+        ),
+        tap(() => (this.status = 'Done')),
+        take(1),
+        catchError((err) => {
+          console.warn(err);
+          this.status = 'Fail';
+          return EMPTY;
+        })
+      )
+      .subscribe(console.log);
+  }
+}
